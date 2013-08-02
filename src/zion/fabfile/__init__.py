b5365d65f68ea315e5161aea11e98344817988a1
Jason Grosz <jgrosz@tinisi.com>
2013-08-02 15:02:45 -0500

    got notes for DHCP install organized and a delete I f'ed up

--- src/zion/fabfile/__init__.py
+++ src/zion/fabfile/__init__.py
@@ -1,8 +1,8 @@
 
 from fabric.api import *
 import os
-import bind
 import dhcp
+import bind
 import puppet
 import foreman
 from zion_config_helper import ZionConfigHelper
@@ -10,14 +10,14 @@ from zion_config_helper import ZionConfigHelper
 @task
 def pre_install():
     # this should be all the steps that must be run as root
-    # os.enable_sudo()
-    os.add_users()
+ 	os.enable_sudo()
+ 	os.add_users()
 
 @task
 def install():
 #    os.ssh_lockdown()
-    os.update()
+#    os.update()
+    dhcp.install()
 #    bind.install()
-#    dhcp.install()
 #    puppet.install()
 #    foreman.install()
